4cd60929a0c10f00c192fe2ce568f545903e3c23
vedansh3805-gif <Vedansh.3805@gmail.com>
2026-03-18 10:05:21 +0530

    UC2: Display Hello with Command-Line Argument

--- src/main/java/com/hello/App.java
+++ src/main/java/com/hello/App.java
@@ -2,6 +2,13 @@ package com.hello;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+
     }
 }
